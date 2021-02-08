b24861991ecbc8a8e41c822725b3e0fb005c7fad
jraleighdev <39661900+jraleighdev@users.noreply.github.com>
2021-02-08 00:03:50 -0600

    Added copy documentation and removed tuple and added ReferenceDto for clarity.

--- CopyTools/CopyHelpers.cs
+++ CopyTools/CopyHelpers.cs
@@ -8,19 +8,38 @@ using System.Threading.Tasks;
 
 namespace InventorWrapper.CopyTools
 {
+    /// <summary>
+    /// Methods for simplifying copy inventor assembly models
+    /// </summary>
     public class CopyHelpers
     {
-        public static void ReplaceReferences(InventorDocument document, List<Tuple<string, string>> paths, List<string> skip = null)
+        /// <summary>
+        /// Replace the references in the document
+        /// </summary>
+        /// <param name="document"></param>
+        /// <param name="referenceData"></param>
+        /// <param name="skip"></param>
+        public static void ReplaceReferences(InventorDocument document, List<ReferenceDto> referenceData, List<string> skip = null)
         {
-            Recurse(document._document.File.ReferencedFileDescriptors, paths);
+            Recurse(document._document.File.ReferencedFileDescriptors, referenceData);
         }
 
-        private static void Recurse(FileDescriptorsEnumerator refs, List<Tuple<string, string>> paths, List<string> skip = null)
+        /// <summary>
+        /// Recursive method that will replace all the references
+        /// In the assembly document and all the child assembly documents
+        /// </summary>
+        /// <param name="refs"></param>
+        /// <param name="referenceData"></param>
+        /// <param name="skip"></param>
+        private static void Recurse(FileDescriptorsEnumerator refs, List<ReferenceDto> referenceData, List<string> skip = null)
         {
+            // Loop through the references in the document
             foreach (FileDescriptor r in refs)
             {
+                // get the full path of the file
                 var path = r.FullFileName;
 
+                // If any of the files are in skip then continue
                 if (skip != null && skip.Count > 0)
                 {
                     if (skip.Any(x => r.FullFileName.ToUpper().Contains(x.ToUpper())))
@@ -29,17 +48,23 @@ namespace InventorWrapper.CopyTools
                     }
                 }
 
-                var refPath = paths.FirstOrDefault(x => x.Item1.ToUpper() == path.ToUpper());
+                // Get the file name that matches the file ref
+                var refPath = referenceData.FirstOrDefault(x => x.OriginalReference.ToUpper() == path.ToUpper());
 
+                // if not found then continue
                 if (refPath == null) continue;
 
-                r.ReplaceReference(refPath.Item2);
+                // Replace the reference with item 2
+                r.ReplaceReference(refPath.NewReference);
 
+                // If the document is an assembly document
                 if (r.ReferencedFileType == FileTypeEnum.kAssemblyFileType)
                 {
+                    // Get the reference file
                     Inventor.File file = r.ReferencedFile;
 
-                    Recurse(file.ReferencedFileDescriptors, paths, skip);
+                    // Recurse through the sub assembly files
+                    Recurse(file.ReferencedFileDescriptors, referenceData, skip);
                 }
             }
         }
